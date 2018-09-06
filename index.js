324e5c3cb84fc6029eeb1b7cf0d3fdda160026e9
Itamar Held <itamar.held@pyramidanalytics.com>
2018-09-06 11:47:18 +0300

    refactor

--- index.js
+++ index.js
@@ -1,44 +1,27 @@
 const fs = require('fs');
-const { groupBy, prop, pickBy, head, compose, find, equals, map, tap, keys, sum, chain, forEach } = require('ramda');
+const { groupBy, prop, pick, head, compose, find, equals, map, tap, keys, sum, chain, forEach } = require('ramda');
 
 const visitedModules = new Set();
 
 const rootId = 1995;
 
-const content = require('F:/New/G2/PyramidG2/clientx/webpack-stats.json', 'utf8');
+const content = require('F:/New/G2/PyramidG2/clientx/webpack-stats.json');
 
 // const getReasons = compose(prop('reasons'), head);
 // const findReason = id => find(compose(equals(id), prop('moduleId')));
 
-const modules = groupBy(prop('id'), content.modules);
-const modulesByIssuerId = groupBy(prop('issuerId', content.modules));
+const modules = map(pick(['id', 'name', 'issuerId', 'size']), content.modules);
+const modulesById = compose(map(head), groupBy(prop('id')))(modules);
+const modulesByIssuerId = groupBy(prop('issuerId'), modules);
 
-const moduleById = id => head(modules[id]);
+const getModuleById = id => modulesById[id];
 
-const isDependancy = id =>
-	compose(
-		equals(id),
-		prop('issuerId')
-	);
-const getDependancies = id =>
-	compose(
-		keys,
-		pickBy(
-			compose(
-				isDependancy(id),
-				head
-			)
-		)
-	);
+const getDependancies = id => modulesByIssuerId[id] || [];
 
-function getModuleDependancies(moduleId, cnt) {
-	if (cnt === 0) {
-		return 0;
-	}
-
-	const dependencies = getDependancies(Number(moduleId))(modules);
-	const unvisitedIds = dependencies.filter(d => !visitedModules.has(d));
-	const unvisited = unvisitedIds.map(moduleById);
+function getModuleDependencies(moduleId) {
+    const dependencies = getDependancies(Number(moduleId));
+    const unvisited = dependencies.filter(d => !visitedModules.has(d.id));
+    const unvisitedIds = unvisited.map(prop('id'));
 	const calcTotalDependanciesSize = compose(
 		sum,
 		map(prop('size'))
@@ -48,7 +31,7 @@ function getModuleDependancies(moduleId, cnt) {
 	// add new unvisited
 	forEach(id => visitedModules.add(id), unvisitedIds);
 
-	return totalSize + sum(chain(id => getModuleDependancies(id, cnt - 1), unvisitedIds));
+	return totalSize + sum(chain(id => getModuleDependencies(id), unvisitedIds));
 }
 
-console.log(getModuleDependancies(rootId, 2));
+console.log(getModuleDependencies(rootId));
