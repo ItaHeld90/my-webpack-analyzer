75ba48c0af3d3ad7fa2fa6b57c983d710699c367
Itamar Held <itamar.held@pyramidanalytics.com>
2018-09-06 12:12:04 +0300

    calc sizes

--- index.js
+++ index.js
@@ -4,30 +4,39 @@ const rootId = 1995;
 
 const content = require('F:/New/G2/PyramidG2/clientx/webpack-stats.json');
 
-// const getReasons = compose(prop('reasons'), head);
-// const findReason = id => find(compose(equals(id), prop('moduleId')));
-
 const modules = map(pick(['id', 'name', 'issuerId', 'size']), content.modules);
-const modulesById = compose(map(head), groupBy(prop('id')))(modules);
+const modulesById = compose(
+	map(head),
+	groupBy(prop('id'))
+)(modules);
 const modulesByIssuerId = groupBy(prop('issuerId'), modules);
+
 const getDependancies = id => modulesByIssuerId[id] || [];
 
 const visitedModules = new Set();
+const result = {};
 
-function getModuleDependencies(moduleId) {
-    const dependencies = getDependancies(Number(moduleId));
-    const unvisited = dependencies.filter(d => !visitedModules.has(d.id));
-    const unvisitedIds = unvisited.map(prop('id'));
+function calcSizes(moduleId) {
+	const dependencies = getDependancies(Number(moduleId));
+	const unvisited = dependencies.filter(d => !visitedModules.has(d.id));
+	const unvisitedIds = unvisited.map(prop('id'));
 	const calcTotalDependanciesSize = compose(
 		sum,
 		map(prop('size'))
 	);
-	const totalSize = calcTotalDependanciesSize(unvisited);
+	const dependenciesSize = calcTotalDependanciesSize(unvisited);
 
 	// add new unvisited
 	forEach(id => visitedModules.add(id), unvisitedIds);
 
-	return totalSize + sum(chain(getModuleDependencies, unvisitedIds));
+	const totalSize = dependenciesSize + sum(chain(calcSizes, unvisitedIds));
+
+	if (totalSize > 0) {
+		result[moduleId] = totalSize;
+    }
+
+	return totalSize;
 }
 
-console.log(getModuleDependencies(rootId));
+console.log(calcSizes(rootId));
+console.log(result);
