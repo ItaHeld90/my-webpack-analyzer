3bc412937ac33f24eb285792b74d3a7fe4524de9
Itamar Held <itamar.held@pyramidanalytics.com>
2018-09-17 13:42:16 +0300

    add react typescript and webpack

---
+++ src/components/AnalyzerMain.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+import { observer } from 'mobx-react';
+
+export interface AnalyzerMainProps {}
+
+@observer
+export class AnalyzerMain extends React.Component<AnalyzerMainProps, {}> {
+	render() {
+		return <div />;
+	}
+}
---
+++ src/index.tsx
@@ -0,0 +1,9 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+
+import { AnalyzerMain } from "./components/AnalyzerMain";
+
+ReactDOM.render(
+    <AnalyzerMain />,
+    document.getElementById("example")
+);
